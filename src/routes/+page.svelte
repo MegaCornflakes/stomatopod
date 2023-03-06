a65133c735fc69461b99d31e608bc6a2cd4e4fb0
Aaron Apfeldorf <aapfeldorf@gmail.com>
2023-03-06 15:11:54 -0500

    First Expand

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	let canvas: HTMLCanvasElement
+	let image: ImageData
+
+	function square(e: MouseEvent) {
+		const ctx = canvas.getContext('2d')
+	}
+
+	function expand(dimensionalImage: number[][][]): number[] {
+		return dimensionalImage
+			.reduce((colors, row) => colors.concat(row), new Array(0))
+			.reduce((flat, color) => flat.concat([...color, 255]), [])
+	}
+
+	onMount(() => {
+		const ctx = canvas.getContext('2d')
+		if (ctx) {
+			//image = ctx.createImageData(canvas.width, canvas.height)
+			let working: number[][][] = []
+			let row: number[][] = []
+			for (let x = 0; x < 500; x++) {
+				for (let y = 0; y < 500; y++) {
+					row.push([
+						(x / 500) * 255,
+						(y / 500) * 255,
+						(1 - Math.sqrt((x / 500) ** 2 + (y / 500) ** 2)) * 255,
+					])
+				}
+				working.push(row)
+			}
+			console.log(working.length)
+			image = new ImageData(expand(working), 500, 500)
+			ctx.putImageData(image, 0, 0)
+			console.log('Canvas Ready')
+		}
+	})
+</script>
+
+<div id="interactive">
+	<canvas bind:this={canvas} on:mousedown={square} width="500" height="500">
+		Drawing Canvas
+	</canvas>
+</div>
+
+<svelte:head>
+	<style>
+		html,
+		body {
+			height: 100%;
+		}
+
+		body {
+			margin: 0;
+		}
+	</style>
+</svelte:head>
+
+<style>
+	#interactive {
+		background-color: #efefef;
+		width: 100%;
+		height: 100%;
+	}
+
+	canvas {
+		background-color: white;
+		margin-inline: auto;
+	}
+</style>
