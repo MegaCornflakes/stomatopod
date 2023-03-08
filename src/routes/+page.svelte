17d6da110537cf9aae4744184896ee87c0c8377c
Aaron Apfeldorf <aapfeldorf@gmail.com>
2023-03-08 14:21:40 -0500

    Aghhh i fixed the expand function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,18 @@
 	}
 
 	function expand(dimensionalImage: number[][][]): number[] {
-		return dimensionalImage
-			.reduce((colors, row) => colors.concat(row), new Array(0))
-			.reduce((flat, color) => flat.concat([...color, 255]), [])
+		let flattened = dimensionalImage.flat()
+		flattened.map((x) => x.push(255))
+		return flattened.flat()
 	}
 
 	onMount(() => {
 		const ctx = canvas.getContext('2d')
 		if (ctx) {
-			//image = ctx.createImageData(canvas.width, canvas.height)
+			image = ctx.createImageData(canvas.width, canvas.height)
 			let working: number[][][] = []
-			let row: number[][] = []
 			for (let x = 0; x < 500; x++) {
+				let row: number[][] = []
 				for (let y = 0; y < 500; y++) {
 					row.push([
 						(x / 500) * 255,
@@ -30,10 +30,24 @@
 				}
 				working.push(row)
 			}
-			console.log(working.length)
-			image = new ImageData(expand(working), 500, 500)
+			console.log(`${working.length}x${working[0].length}`)
+			expand(working).map((n, i) => (image.data[i] = n))
 			ctx.putImageData(image, 0, 0)
 			console.log('Canvas Ready')
+			setInterval(() => {
+				for (let x = 0; x < 500; x++) {
+					for (let y = 0; y < 500; y++) {
+						working[x][y] = [
+							Math.max(Math.sin(x / 500 + new Date().getSeconds()) * 255, 0),
+							(y / 500) * 255,
+							(1 - Math.sqrt((x / 500) ** 2 + (y / 500) ** 2)) * 255,
+						]
+					}
+				}
+				//console.log(`${working.length}x${working[0].length}`)
+				expand(working).map((n, i) => (image.data[i] = n))
+				ctx.putImageData(image, 0, 0)
+			}, 33)
 		}
 	})
 </script>
